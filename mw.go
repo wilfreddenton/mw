6d59990ab8c06bfac9f9a240bb50426c1b78ee21
wilfred <wilfred.denton2@ibm.com>
2016-09-18 02:48:05 -0400

    first commit

---
+++ mw.go
@@ -0,0 +1,29 @@
+package mw
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func compose(ms []Ware, h http.Handler) http.Handler {
+	if len(ms) == 0 {
+		return h
+	} else {
+		return ms[0](compose(ms[1:], h))
+	}
+}
+
+func CreateEndpoints(r *mux.Router, es []Endpoint) {
+	for _, e := range es {
+		r.Handle(e.Path, compose(e.Middlewares, e.Handler)).Methods(e.Methods...)
+	}
+}
+
+type Ware func(http.Handler) http.Handler
+
+type Endpoint struct {
+	Path        string
+	Middlewares []Ware
+	Handler     appHandler
+	Methods     []string
+}
---
+++ mw_test.go
@@ -0,0 +1,77 @@
+package mw
+
+import (
+	"fmt"
+	"github.com/gorilla/context"
+	"github.com/gorilla/mux"
+	"net/http"
+	"testing"
+)
+
+type Error struct {
+	Message string
+	Code    int
+	Error   error
+}
+
+type appHandler func(w http.ResponseWriter, r *http.Request) *Error
+
+func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if e := fn(w, r); e != nil {
+		http.Error(w, e.Message, e.Code)
+	}
+}
+
+// middlewares
+func printer(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if v, ok := context.GetOk(r, "text"); ok {
+			fmt.Println(v)
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func writer1(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		context.Set(r, "text", "Hello World!")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func writer2(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		context.Set(r, "text", "h e l l o w o r l d")
+		next.ServeHTTP(w, r)
+	})
+}
+
+// handlers
+func endpoint1Handler(w http.ResponseWriter, r *http.Request) *Error {
+	w.Write([]byte("endpoint 1"))
+	return nil
+}
+
+func endpoint2Handler(w http.ResponseWriter, r *http.Request) *Error {
+	w.Write([]byte("endpoint 2"))
+	return nil
+}
+
+func Test(t *testing.T) {
+	e1 := Endpoint{
+		"/e1",
+		[]Ware{writer1, printer},
+		endpoint1Handler,
+		[]string{"GET"},
+	}
+	e2 := Endpoint{
+		"/e2",
+		[]Ware{writer2, printer},
+		endpoint2Handler,
+		[]string{"GET"},
+	}
+	es := []Endpoint{e1, e2}
+	r := mux.NewRouter().StrictSlash(true)
+	CreateEndpoints(r, es)
+	http.ListenAndServe(":9999", r)
+}
