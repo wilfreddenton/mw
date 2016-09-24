9161317c1603e426a0568fe994a829ad3d9f4141
wilfred <wilfred.denton2@ibm.com>
2016-09-24 18:32:18 -0400

    handlers and wares are now structs instead of just functions. the structs have params that describe the expected inputs and the outputs of the ware. this allows the new compose function to validate the inputs and outputs to make sure blocks are ordered properly

--- mw.go
+++ mw.go
@@ -2,29 +2,77 @@ package mw
 
 import (
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
-func compose(ms []Ware, h http.Handler) http.Handler {
+func isInInputs(inputs []string, input string) bool {
+	for _, i := range inputs {
+		if i == input {
+			return true
+		}
+	}
+	return false
+}
+
+func isValidHandler(inputs []string, h Handler) bool {
+	for _, i := range h.Inputs {
+		if !isInInputs(inputs, i) {
+			log.Printf("%s ware is not receiving input %s", h.Name, i)
+			return false
+		}
+	}
+	return true
+}
+
+func isValidWare(inputs []string, w Ware) bool {
+	for _, i := range w.Inputs {
+		if !isInInputs(inputs, i) {
+			log.Printf("%s ware is not receiving input %s", w.Name, i)
+			return false
+		}
+	}
+	return true
+}
+
+func Compose(ms []Ware, h Handler, inputs []string) http.Handler {
 	if len(ms) == 0 {
-		return h
+		if !isValidHandler(inputs, h) {
+			log.Fatalf("exiting because %s handler is not chained the correct blocks", h.Name)
+		}
+		return h.Fn
 	} else {
-		return ms[0](compose(ms[1:], h))
+		w := ms[0]
+		if !isValidWare(inputs, w) {
+			log.Fatalf("exiting because %s ware is not chained the correct blocks", w.Name)
+		}
+		return w.Fn(Compose(ms[1:], h, append(inputs, w.Outputs...)))
 	}
 }
 
 func CreateEndpoints(r *mux.Router, es []Endpoint, prefix string) {
 	for _, e := range es {
-		r.Handle(prefix+e.Path, compose(append(e.Middlewares, e.Blockwares...), e.Handler)).Methods(e.Methods...)
+		r.Handle(prefix+e.Path, Compose(append(e.Middlewares, e.Blockwares...), e.Handler, []string{})).Methods(e.Methods...)
 	}
 }
 
-type Ware func(http.Handler) http.Handler
+type Ware struct {
+	Name    string
+	Inputs  []string
+	Outputs []string
+	Fn      func(http.Handler) http.Handler
+}
+
+type Handler struct {
+	Name   string
+	Inputs []string
+	Fn     http.Handler
+}
 
 type Endpoint struct {
 	Path        string
 	Middlewares []Ware
 	Blockwares  []Ware
-	Handler     http.Handler
+	Handler     Handler
 	Methods     []string
 }
--- mw_test.go
+++ mw_test.go
@@ -4,11 +4,17 @@ import (
 	"fmt"
 	"github.com/gorilla/context"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-const keyText = "text"
+const (
+	keyText  = "text"
+	textVal1 = "Hello World!"
+	textVal2 = "h e l l o w o r l d"
+)
 
 type Error struct {
 	Message string
@@ -25,57 +31,236 @@ func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 // middlewares
-func printer(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if v, ok := context.GetOk(r, keyText); ok {
-			fmt.Println(v)
-		}
-		next.ServeHTTP(w, r)
-	})
+var printer = Ware{
+	Name:   "printer",
+	Inputs: []string{keyText},
+	Fn: func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if v, ok := context.GetOk(r, keyText); ok {
+				fmt.Println(v)
+			}
+			next.ServeHTTP(w, r)
+		})
+	},
 }
 
-func writer1(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		context.Set(r, keyText, "Hello World!")
-		next.ServeHTTP(w, r)
-	})
+var writer1 = Ware{
+	Name:    "writer1",
+	Outputs: []string{keyText},
+	Fn: func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			context.Set(r, keyText, textVal1)
+			next.ServeHTTP(w, r)
+		})
+	},
 }
 
-func writer2(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		context.Set(r, keyText, "h e l l o w o r l d")
-		next.ServeHTTP(w, r)
-	})
+var writer2 = Ware{
+	Name:    "writer2",
+	Outputs: []string{keyText},
+	Fn: func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			context.Set(r, keyText, textVal2)
+			next.ServeHTTP(w, r)
+		})
+	},
 }
 
 // handlers
-func endpoint1Handler(w http.ResponseWriter, r *http.Request) *Error {
-	w.Write([]byte("endpoint 1"))
-	return nil
+var handler1 = Handler{
+	Name:   "handler1",
+	Inputs: []string{keyText},
+	Fn: appHandler(func(w http.ResponseWriter, r *http.Request) *Error {
+		t := context.Get(r, keyText).(string)
+		w.Write([]byte(t))
+		return nil
+	}),
 }
 
-func endpoint2Handler(w http.ResponseWriter, r *http.Request) *Error {
-	w.Write([]byte("endpoint 2"))
-	return nil
+var handler2 = Handler{
+	Name:   "handler2",
+	Inputs: []string{keyText},
+	Fn: appHandler(func(w http.ResponseWriter, r *http.Request) *Error {
+		t := context.Get(r, keyText).(string)
+		w.Write([]byte(t))
+		return nil
+	}),
 }
 
-func Test(t *testing.T) {
-	e1 := Endpoint{
-		"/e1",
-		[]Ware{writer1},
-		[]Ware{printer},
-		appHandler(endpoint1Handler),
-		[]string{"GET"},
+func TestIsInInputs(t *testing.T) {
+	i1 := []string{"hello", "world"}
+	i2 := []string{"hello", "you", "there"}
+	if !isInInputs(i1, "hello") {
+		t.Errorf("should be in inputs")
 	}
-	e2 := Endpoint{
-		"/e2",
-		[]Ware{writer2},
-		[]Ware{printer},
-		appHandler(endpoint2Handler),
-		[]string{"GET"},
+	if isInInputs(i2, "world") {
+		t.Errorf("should not be in inputs")
 	}
-	es := []Endpoint{e1, e2}
-	r := mux.NewRouter().StrictSlash(true)
-	CreateEndpoints(r, es, "/api/v1")
-	http.ListenAndServe(":9999", r)
+}
+
+func TestIsValidHandler(t *testing.T) {
+	inputs := []string{"hello", "world"}
+	h := Handler{Name: "h1", Inputs: inputs}
+	if !isValidHandler(inputs, h) {
+		t.Error("should be a valid handler")
+	}
+	h = Handler{Name: "h2", Inputs: []string{"foo"}}
+	if isValidHandler(inputs, h) {
+		t.Error("should be an invalid handler")
+	}
+}
+
+func TestIsValidWare(t *testing.T) {
+	inputs := []string{"hello", "world"}
+	w := Ware{Name: "w1", Inputs: inputs}
+	if !isValidWare(inputs, w) {
+		t.Error("should be a valid ware")
+	}
+	w = Ware{Name: "w2", Inputs: []string{"foo"}}
+	if isValidWare(inputs, w) {
+		t.Error("should be an invalid ware")
+	}
+}
+
+func TestCompose(t *testing.T) {
+	key1 := "key1"
+	key2 := "key2"
+	key3 := "key3"
+	key4 := "key4"
+	val1 := "val1"
+	val2 := "val2"
+	val3 := "val3"
+	val4 := "val4"
+	ws := []Ware{
+		Ware{
+			Name:    "w1",
+			Outputs: []string{key1, key2},
+			Fn: func(next http.Handler) http.Handler {
+				return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					context.Set(r, key1, val1)
+					context.Set(r, key2, val2)
+					next.ServeHTTP(w, r)
+				})
+			},
+		},
+		Ware{
+			Name:    "w2",
+			Inputs:  []string{key1, key2},
+			Outputs: []string{key3},
+			Fn: func(next http.Handler) http.Handler {
+				return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					if _, ok := context.GetOk(r, key1); !ok {
+						t.Errorf("%s not set", key1)
+					}
+					if _, ok := context.GetOk(r, key2); !ok {
+						t.Errorf("%s not set", key2)
+					}
+					context.Set(r, key3, val3)
+					next.ServeHTTP(w, r)
+				})
+			},
+		},
+		Ware{
+			Name:    "w3",
+			Inputs:  []string{key1, key2, key3},
+			Outputs: []string{key4},
+			Fn: func(next http.Handler) http.Handler {
+				return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					if _, ok := context.GetOk(r, key1); !ok {
+						t.Errorf("%s not set", key1)
+					}
+					if _, ok := context.GetOk(r, key2); !ok {
+						t.Errorf("%s not set", key2)
+					}
+					if _, ok := context.GetOk(r, key3); !ok {
+						t.Errorf("%s not set", key3)
+					}
+					context.Set(r, key4, val4)
+					next.ServeHTTP(w, r)
+				})
+			},
+		},
+	}
+	h := Handler{
+		Name:   "h1",
+		Inputs: []string{key4},
+		Fn: appHandler(func(w http.ResponseWriter, r *http.Request) *Error {
+			if _, ok := context.GetOk(r, key4); !ok {
+				t.Errorf("%s not set", key4)
+			}
+			w.Write([]byte(val4))
+			return nil
+		}),
+	}
+	chain := Compose(ws, h, []string{})
+	path := "/testcompose"
+	router := mux.NewRouter()
+	router.Handle(path, chain)
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest(http.MethodGet, path, nil)
+	if err != nil {
+		t.Fatal("could not create request")
+	}
+	router.ServeHTTP(w, r)
+	if w.Code != http.StatusOK {
+		t.Errorf("got code %v; want %v", w.Code, http.StatusOK)
+	}
+}
+
+func TestCreateEndpoint(t *testing.T) {
+	es := []Endpoint{
+		Endpoint{
+			"/e1",
+			[]Ware{writer1},
+			[]Ware{printer},
+			handler1,
+			[]string{"GET"},
+		},
+		Endpoint{
+			"/e2",
+			[]Ware{writer2},
+			[]Ware{printer},
+			handler2,
+			[]string{"GET"},
+		},
+	}
+	prefix := "/api/v1"
+	router := mux.NewRouter()
+	CreateEndpoints(router, es, prefix)
+	// TEST e1
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest(http.MethodGet, prefix+"/e1", nil)
+	if err != nil {
+		t.Fatal("could not create request")
+	}
+	router.ServeHTTP(w, r)
+	if w.Code != http.StatusOK {
+		t.Fatalf("got code %v; want %v", w.Code, http.StatusOK)
+	}
+	body, err := ioutil.ReadAll(w.Body)
+	if err != nil {
+		t.Error("body could not be read")
+	}
+	val := string(body)
+	if val != textVal1 {
+		t.Errorf("got body %v; want %v", val, textVal1)
+	}
+	// TEST e2
+	w = httptest.NewRecorder()
+	r, err = http.NewRequest(http.MethodGet, prefix+"/e2", nil)
+	if err != nil {
+		t.Fatal("could not create request")
+	}
+	router.ServeHTTP(w, r)
+	if w.Code != http.StatusOK {
+		t.Fatalf("got code %v; want %v", w.Code, http.StatusOK)
+	}
+	body, err = ioutil.ReadAll(w.Body)
+	if err != nil {
+		t.Error("body could not be read")
+	}
+	val = string(body)
+	if val != textVal2 {
+		t.Errorf("got body %v; want %v", val, textVal2)
+	}
 }
